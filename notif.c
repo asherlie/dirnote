06fc55a4c55c3e7a11fad6b9be2872a47a9a89f2
Asher Lieber <asherlie@buffalo.edu>
2019-02-18 23:52:46 -0500

    added stubbed out functions

--- notif.c
+++ notif.c
@@ -4,6 +4,8 @@
 #include <dirent.h>
 
 struct finf{
+      char fname[255];
+      unsigned char namelen;
       time_t edit_t;
       ino_t file_no;
 };
@@ -19,7 +21,23 @@ void fsys_init(struct fsys* fs){
       fs->files = malloc(sizeof(struct finf)*fs->cap);
 }
 
-_Bool checker(char* fpath){
+struct finf finf_build(time_t edit_t, ino_t file_no, char* fname, unsigned char namelen){
+      struct finf f;
+      f.edit_t = edit_t;
+      f.file_no = file_no;
+      strncpy(f.fname, fname, namelen);
+      return f;
+}
+
+_Bool fsys_insert(struct fsys* fs, struct finf f){
+      if(fs->n == fs->cap){
+      }
+      fs->files[fs->n++] = f;
+}
+
+// if(!fs) a new fsys* is malloc'd
+struct fsys* fsys_build(struct fsys* fs, char* fpath){
+      if(!fs)fs = malloc(sizeof(struct fsys));
       struct stat attr; // attr.st_mtime 
       DIR* d = opendir(fpath);
       struct dirent* dir;
@@ -29,8 +47,15 @@ _Bool checker(char* fpath){
             }
             closedir(d);
       }
-      else return 1;
-      return 0;
+      else return NULL:
+      return fs
+}
+
+int fsys_cmp(struct fsys* fs0, struct fsys* fs1){
+}
+
+// merges fs_src into fs_dest
+void fsys_merge(struct fsys* fs_dest, struct fsys* fs_src){
 }
 
 int main(int argc, char** argv){
