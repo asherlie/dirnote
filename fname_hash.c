da007e7787947af366ba835f07dc863e45b3c77c
asherlie <lieber.asher@gmail.com>
2019-02-27 15:12:46 -0500

    fixed memory leaks in fname hash

--- fname_hash.c
+++ fname_hash.c
@@ -6,7 +6,8 @@
 struct fname* fname_init(struct fname* fn, int buckets){
       if(!fn)fn = malloc(sizeof(struct fname));
       fn->bux = buckets;
-      fn->filenames = calloc(sizeof(struct fname_entry*), fn->bux);
+      // fn->filenames = calloc(sizeof(struct fname_entry*), fn->bux);
+      fn->filenames = calloc(sizeof(struct fname_lst), fn->bux);
       fn->in_use = malloc(sizeof(int)*(fn->bux+1));
       memset(fn->in_use, -1, sizeof(int)*(fn->bux+1));
       fn->n = 0;
@@ -17,15 +18,16 @@ void add_file_to_fhash(struct fname* fn, ino_t file_no, char* fname){
       int ind = file_no%fn->bux;
       struct fname_entry* cur;
       // if this is the first entry of bucket ind
-      if(!fn->filenames[ind]){
-            cur = fn->filenames[ind] = malloc(sizeof(struct fname_entry));
-            fn->filenames[ind]->first = fn->filenames[ind]->last = cur;
+      if(!fn->filenames[ind].fnames){
+            cur = fn->filenames[ind].fnames = malloc(sizeof(struct fname_entry));
+            fn->filenames[ind].first = fn->filenames[ind].last = cur;
             fn->in_use[fn->n++] = ind;
       }
       else{
             if(get_fname(fn, file_no))return;
-            cur = fn->filenames[ind]->last->next = malloc(sizeof(struct fname_entry));
-            fn->filenames[ind]->last = cur;
+            cur = fn->filenames[ind].last->next = malloc(sizeof(struct fname_entry));
+            fn->filenames[ind].last = cur;
+            fn->filenames[ind].last = cur;
       }
       cur->next = NULL;
       cur->fname = fname;
@@ -34,15 +36,23 @@ void add_file_to_fhash(struct fname* fn, ino_t file_no, char* fname){
 
 char* get_fname(struct fname* fn, ino_t file_no){
       int ind = file_no%fn->bux;
-      for(struct fname_entry* fe = fn->filenames[ind]->first; fe; fe = fe->next)
+      for(struct fname_entry* fe = fn->filenames[ind].first; fe; fe = fe->next)
             if(fe->file_no == file_no)return fe->fname;
       return NULL;
 }
 
 void fhash_free(struct fname* fn){
+      struct fname_entry* prev;
       for(int i = 0; fn->in_use[i] != -1; ++i){
-            free(fn->filenames[fn->in_use[i]]->fname);
-            free(fn->filenames[fn->in_use[i]]);
+            prev = NULL;
+            for(struct fname_entry* fe = fn->filenames[fn->in_use[i]].first;
+                fe; fe = fe->next){
+                  if(prev)free(prev);
+                  free(fe->fname);
+                  prev = fe;
+            }
+            free(prev);
       }
       free(fn->filenames);
+      free(fn->in_use);
 }
--- fname_hash.h
+++ fname_hash.h
@@ -1,20 +1,26 @@
 #include <dirent.h>
+#include <sys/stat.h>
 #ifndef _FNAME_HASH
 #define _FNAME_HASH
 
 struct fname_entry{
       char* fname;
       ino_t file_no;
-      struct fname_entry* next, * first, * last;
+      struct fname_entry* next, * last;
+};
+
+struct fname_lst{
+      struct fname_entry* fnames, * first, * last;
 };
 
 struct fname{
       int bux, n, * in_use;
-      struct fname_entry** filenames;
+      struct fname_lst* filenames;
 };
 
 struct fname* fname_init(struct fname* fn, int buckets);
 void add_file_to_fhash(struct fname* fn, ino_t file_no, char* fname);
 char* get_fname(struct fname* fn, ino_t file_no);
+// fhash_free frees all strings in fn->filenames[ind].fnames->fname
 void fhash_free(struct fname* fn);
 #endif
